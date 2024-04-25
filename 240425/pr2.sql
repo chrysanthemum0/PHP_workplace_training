3a62cf22785aa5f147706638eea6e125cd8ed407
JIN HONG KIM <77311928+chrysanthemum0@users.noreply.github.com>
2024-04-25 18:02:26 +0900

    Add files via upload
    update sql

---
+++ 240425/pr2.sql
@@ -0,0 +1,24 @@
+#접속 방법 및 경로
+$ cd ../../mysql/bin && ./mysql -u root -c DB이름
+
+#1
+SELECT * FROM product
+WHERE cost >= 20000;
+
+#2
+SELECT cust_id, cust_name FROM customer
+WHERE fax IS NULL;
+
+
+#3
+SELECT sales_no, psales_no, prod_id, price FROM sales
+WHERE psales_no BETWEEN 110 AND 119 ORDER BY price DESC;
+
+#4
+SELECT prod_id, prod_name, (cost * discount) FROM product
+WHERE prod_id IN (102, 104, 106)
+ORDER BY (cost * discount) DESC;
+
+#5
+SELECT cust_id, cust_address, delivery_date FROM packedsales
+WHERE delivery_date = '1996' AND cust_address  LIKE '%渋谷区%';
